28cc9a80e53c22dfc7b9e6eebe2617454db6094c
kibzrael <kibraphael7@gmail.com>
2022-10-02 12:25:55 +0300

    dynamic background

--- app.vue
+++ app.vue
@@ -1,8 +1,27 @@
 <template>
-  <div>
+  <div
+    class="app"
+    :style="{
+      'background-image':
+        'url(/assets/images/background-' + page + '-desktop.jpg)',
+    }"
+  >
     <NuxtLayout>
-      <Navbar />
+      <Navbar v-model="page" />
       <NuxtPage class="page" />
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+let url = useRoute().name;
+let urlName: string = url == "index" ? "home" : url.toString();
+let page = ref(urlName);
+</script>
+
+<style lang="scss">
+.app{
+  min-height: 100vh;
+  background-size: cover;
+}
+</style>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,18 +4,31 @@
     <nav>
       <ol>
         <li>
-          <NuxtLink class="link active" to="/"><span>00</span> HOME</NuxtLink>
+          <NuxtLink @click="selectTab(0)" :class="'link' + getActive(0)" to="/"
+            ><span>00</span> HOME</NuxtLink
+          >
         </li>
         <li>
-          <NuxtLink class="link" to="/destination"
+          <NuxtLink
+            @click="selectTab(1)"
+            :class="'link' + getActive(1)"
+            to="/destination"
             ><span>01</span> DESTINATION</NuxtLink
           >
         </li>
         <li>
-          <NuxtLink class="link" to="/crew"><span>02</span> CREW</NuxtLink>
+          <NuxtLink
+            @click="selectTab(2)"
+            :class="'link' + getActive(2)"
+            to="/crew"
+            ><span>02</span> CREW</NuxtLink
+          >
         </li>
         <li>
-          <NuxtLink class="link" to="/technology"
+          <NuxtLink
+            @click="selectTab(3)"
+            :class="'link' + getActive(3)"
+            to="/technology"
             ><span>03</span> TECHNOLOGY</NuxtLink
           >
         </li>
@@ -23,4 +36,21 @@
     </nav>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+let props = defineProps(["modelValue"]);
+let emits = defineEmits(["update:modelValue"]);
+
+let url = props.modelValue;
+let pages = { home: 0, destination: 1, crew: 3, technology: 4 };
+let pageIndex = ref(pages[url]);
+
+function selectTab(index) {
+  pageIndex.value = index;
+  let pageName = Object.keys(pages)[index];
+  emits("update:modelValue", pageName);
+}
+
+function getActive(index): string {
+  return index == pageIndex.value ? " active" : "";
+}
+</script>
