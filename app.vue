4dd5b01ccb4a55e752b05fc0d2fa1ca74494ab8f
kibzrael <kibraphael7@gmail.com>
2022-12-02 10:55:12 +0300

    meta tags

--- app.vue
+++ app.vue
@@ -3,9 +3,8 @@
     class="app"
     :style="{
       'background-image':
-        'url(/assets/images/background-' + page + '-desktop.jpg)',
-    }"
-  >
+        'url(/assets/images/background-' + page + '-' + screen + '.jpg)',
+    }">
     <NuxtLayout>
       <Navbar v-model="page" />
       <NuxtPage class="page" />
@@ -17,10 +16,30 @@
 let url = useRoute().name;
 let urlName: string = url == "index" ? "home" : url.toString();
 let page = ref(urlName);
+
+let screen = ref("desktop");
+
+function setBackground() {
+  if (process.client) {
+    let width = window.innerWidth;
+    console.log(width);
+    if (width > 864) {
+      screen.value = "desktop";
+    } else if (width > 576) {
+      screen.value = "tablet";
+    } else {
+      screen.value = "mobile";
+    }
+  }
+}
+
+onMounted(() => {
+  window.addEventListener("resize", setBackground);
+});
 </script>
 
 <style lang="scss">
-.app{
+.app {
   min-height: 100vh;
   background-size: cover;
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
+export default {
   css: ["@/assets/css/styles.scss"],
-});
+  app: {
+    head: {
+      title: "Frontend Mentor | Space tourism website",
+      meta: [],
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/assets/favicon-32x32.png",
+        },
+      ],
+    },
+  },
+};
